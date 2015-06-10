fcdd95342a29d870aa98d481506b222324fd1c33
Jannes Nagel <j.nagel@happy-kraken.com>
2015-06-10 19:28:17 +0200

    Command line support

--- aufgabe01/JaNagel/Aufgabe01.py
+++ aufgabe01/JaNagel/Aufgabe01.py
@@ -1,9 +1,10 @@
+import sys
+
 def fizzBuzz(number):
     for i in range(number):
         r= ('Fizz' if i%3==0 else '')+('Buzz' if i%5==0 else '')
         yield r if r!='' else str(i)
-    return
 
-f=fizzBuzz(100)
+f=fizzBuzz(sys.argv[1] if len(sys.argv)>1 else 100)
 for i in f:
     print i
