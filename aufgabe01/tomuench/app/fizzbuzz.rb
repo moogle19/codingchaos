bdfef06325b39d5333a49aba295b1df6e4f42381
Tobias Münch <to.muench@muench-its.de>
2015-06-10 19:40:10 +0200

    Task02 finish

--- aufgabe01/tomuench/app/fizzbuzz.rb
+++ aufgabe01/tomuench/app/fizzbuzz.rb
@@ -5,7 +5,7 @@ class Fizzbuzz
   end
   def render(number)
     return number.to_s if number == 0
-    return "#{@fizz}#{@buzz}" if (number % 3) == 0 && (number % 5)  == 0
+    return "#{@fizz}#{@buzz}" if (number % 15)  == 0
     return @fizz if number % 3  == 0
     return @buzz if number % 5  == 0
     return number.to_s
