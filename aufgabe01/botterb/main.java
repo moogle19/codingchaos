22a55c6f6cc05fd82ac6169355e21000352d82d7
Benjamin Ottersbach <benjamin@zahlz.com>
2015-06-10 19:23:20 +0200

    foo

--- aufgabe01/botterb/main.java
+++ aufgabe01/botterb/main.java
@@ -15,8 +15,8 @@ public class CodingChaos_01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    //            int i = Integer.valueOf(args[1]);
-            int i = 15;
+    int i = Integer.valueOf(args[1]);
+    
         		for(int j = 1; j<=i; j++){
 			if(j % 3 == 0){
 				if(j %5 == 0) {
