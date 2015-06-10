7cb4f59feabdf48fde70b0a6377c4714c5d2caf8
themightyoarfish <rdiederichse@uos.de>
2015-06-10 19:15:49 +0200

    file in wrong folder..

--- aufgabe01/fizzbuzz.rb
+++
@@ -1,9 +0,0 @@
-# works for n > 0
-if __FILE__ == $0
-   f = lambda { |i| 
-      return "FIZZBUZZ" if ((i % 5) + (i % 3)) == 0
-      return "FIZZ" if (i % 3) == 0
-      return "BUZZ" if (i % 5) == 0
-   }
-   1.upto(ARGV[0].to_i) { |i| puts(f.call i)}
-end
