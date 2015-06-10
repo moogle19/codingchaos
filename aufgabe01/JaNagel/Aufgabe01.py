a408cb0df08c494b6cce0855602faf79fd0903c7
Jannes Nagel <j.nagel@happy-kraken.com>
2015-06-10 19:13:46 +0200

    Bugfix

--- aufgabe01/JaNagel/Aufgabe01.py
+++ aufgabe01/JaNagel/Aufgabe01.py
@@ -1,6 +1,6 @@
 def fizzBuzz(number):
     for i in range(number):
-        r= 'Fizz' if i%3==0 else ''+'Buzz' if i%5 else ''
+        r= 'Fizz' if i%3==0 else ''+'Buzz' if i%5==0 else ''
         yield r if r!='' else str(i)
     return
 
