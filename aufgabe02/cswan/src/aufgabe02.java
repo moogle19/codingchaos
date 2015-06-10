2006bd4038ec9afa2b8034508050900d4b5be08a
Christian Swan <dazzel@online.de>
2015-06-10 19:49:00 +0200

    solve aufgabe01

--- aufgabe02/cswan/src/aufgabe02.java
+++ aufgabe02/cswan/src/aufgabe02.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class aufgabe02 {
+    public static  void main(String[] args) {
+        int length = Integer.parseInt(args[0]);
+        int height = Integer.parseInt(args[1]);
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(length > 0 && height > 0) {
+            if(length > height) {
+                list.add(height);
+                length -= height;
+            }
+            else {
+                list.add(length);
+                height -= length;
+            }
+        }
+
+        System.out.println(list);
+    }
 }
