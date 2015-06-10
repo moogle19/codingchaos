35ebf1592d37d5a6b89f96c06bcebed2fcd69ff7
funnyflowerpot <oberwipf@gmail.com>
2015-06-10 19:13:49 +0200

    aufgabe 1 mehr besser mehr sexy

--- aufgabe01/sriegl/aufgabe1.py
+++ aufgabe01/sriegl/aufgabe1.py
@@ -1,6 +1,3 @@
 import sys
 for n in range(1,int(sys.argv[1])+1):
-  o=''
-  if n%3==0: o='Fizz'
-  if n%5==0: o+='Buzz'
-  print o and o or n
+ print'%s%s'%(n%3==0 and'Fizz'or'',n%5==0 and'Buzz'or'')or n
