fa23c7939a3b130da4cbc72994061ef543486928
Justin Güse <jguese@uos.de>
2015-06-10 20:35:30 +0200

    Working!

--- aufgabe03/jguese/irc.py
+++ aufgabe03/jguese/irc.py
@@ -1,20 +1,38 @@
-import socket
+# Import some necessary libraries.
+import socket 
 
-network = 'irc.freenode.net'
-port = 6667
-irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
-irc.connect ( ( network, port ) )
-print irc.recv ( 4096 )
+# Some basic variables used to configure the bot        
+server = "irc.freenode.net" # Server
+channel = "#codingchaos" # Channel
+botnick = "GueseBot" # Your bots nick
 
 
-irc.send ( 'USER jguese Hostname Servername Justin' )
-irc.send ( 'NICK JustinGuese' )
-irc.send ( 'JOIN #codingchaos' )
-#irc.senc('MSG Peace!')
-#irc.send ( 'PRIVMSG #nille :Hello World.\r\n' )
+def ping(): # This is our first function! It will respond to server Pings.
+  ircsock.send("PONG :pingis\n")  
 
-#while True:
-#   data = irc.recv ( 4096 )
-#   if data.find ( 'PING' ) != -1:
-#      irc.send ( 'PONG ' + data.split() [ 1 ] + '\r\n' )
-#   print data
+def sendmsg(chan , msg): # This is the send message function, it simply sends messages to the channel.
+  ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n") 
+
+def joinchan(chan): # This function is used to join channels.
+  ircsock.send("JOIN "+ chan +"\n")
+
+def hello(): # This function responds to a user that inputs "Hello Mybot"
+  ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
+                  
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667
+ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This bot is a result of a tutoral covered on http://shellium.org/wiki.\n") # user authentication
+ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
+
+joinchan(channel) # Join the channel using the functions we previously defined
+
+while 1: # Be careful with these! it might send you to an infinite loop
+  ircmsg = ircsock.recv(2048) # receive data from the server
+  ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
+  print(ircmsg) # Here we print what's coming from the server
+
+  if ircmsg.find("!say") != -1: # If we can find "Hello Mybot" it will call the function hello()
+    hello()
+
+  if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
+    ping()
