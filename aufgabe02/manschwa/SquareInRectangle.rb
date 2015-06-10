7ba71f0383669e37aa197b4eead43b442989286e
Manuel Schwarz <Manuel@83adb936.funky.uni-osnabrueck.de>
2015-06-10 20:04:39 +0200

    Aufgabe02 jetzt auch richtig(er)

--- aufgabe02/manschwa/SquareInRectangle.rb
+++ aufgabe02/manschwa/SquareInRectangle.rb
@@ -4,14 +4,17 @@ puts "============================="
 width = ARGV[0].to_i
 height = ARGV[1].to_i
 
+if width >= height
+  start = height
+else
+  start = width
+end
+
 big_square = width * height
 sqrt = (Math.sqrt big_square).to_i
-for e in sqrt.downto 1
-  if e**2 <= big_square
+for e in start.downto 1
+  while e**2 <= big_square
     puts "#{e}"
     big_square = big_square - e**2
-  end
-  if big_square == e**2
-    puts e
   end
 end
